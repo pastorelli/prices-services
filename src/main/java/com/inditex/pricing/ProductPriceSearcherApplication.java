c67aa4cb9fe7b7a93250d18719b062b10d724907
Gustavo Pastorelli <gpastorelli89@gmail.com>
2025-04-21 18:25:59 +0200

    feat: Initialize Spring Boot project with Maven Wrapper and basic application structure

---
+++ src/main/java/com/inditex/pricing/ProductPriceSearcherApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPriceSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductPriceSearcherApplication.class, args);
+    }
+
+}
